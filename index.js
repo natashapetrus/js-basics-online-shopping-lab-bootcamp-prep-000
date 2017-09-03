35081349a1f8f789302af30a18055a5e40636b72
Natasha Petrus <hello@natashapetrus.com>
2017-09-03 06:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let itemPrice = Math.floor(Math.random() * 100);
+  var newObject = {[item] : itemPrice};
+  cart.push(newObject);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length == 0){
+    console.log('Your shopping cart is empty.');
+  }
+  else{
+    var newString = 'In your cart, you have ';
+    for (let i=0; i<cart.length-1; i++){
+      newString = newString + Object.keys(cart[i])[i] + ' at $' + cart[i][Object.keys(cart[i])[i]];
+    }
+    console.log('In your cart, you have ');
+  }
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0;
+  for (let i=0; i<cart.length; i++){
+    totalPrice += cart[i][Object.keys(cart[i])[i]];
+  }
+  return totalPrice;
 }
 
 function removeFromCart(item) {
