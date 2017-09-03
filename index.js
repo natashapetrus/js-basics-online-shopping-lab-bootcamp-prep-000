d4cff7ff54476adcd0363dfeb6c71239d4a6ef8c
Natasha Petrus <hello@natashapetrus.com>
2017-09-03 06:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ function viewCart() {
     for (let i=0; i<cart.length-1; i++){
       newString = newString + Object.keys(cart[i])[0] + ' at $' + cart[i][Object.keys(cart[i])[0]] + ', ';
     }
+    if (!(cart.length==1)){
+      newString = newString + 'and ';
+    }
+    newString = newString + Object.keys(cart[cart.length-1])[0] + ' at $' + cart[cart.length-1][Object.keys(cart[cart.length-1])[0]] + '.';
     console.log(newString);
   }
 }
